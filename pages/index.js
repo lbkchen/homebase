da89e4b5a32dbc33c1259adb03d98351197118ab
Ken Chen <ken.chen@samsara.com>
2019-12-25 22:07:06 -0800

    Load Bulma styles

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
-import React from 'react'
-import Head from 'next/head'
-import Nav from '../components/nav'
+import React from "react";
+import Head from "next/head";
+import Nav from "../components/nav";
+import "../styles/styles.sass";
 
 const Home = () => (
   <div>
@@ -12,27 +13,16 @@ const Home = () => (
     <Nav />
 
     <div className="hero">
-      <h1 className="title">Welcome to Next.js!</h1>
-      <p className="description">
-        To get started, edit <code>pages/index.js</code> and save to reload.
-      </p>
+      <div class="buttons">
+        <button class="button is-primary">Primary</button>
+        <button class="button is-link">Link</button>
+      </div>
 
-      <div className="row">
-        <a href="https://nextjs.org/docs" className="card">
-          <h3>Documentation &rarr;</h3>
-          <p>Learn more about Next.js in the documentation.</p>
-        </a>
-        <a href="https://nextjs.org/learn" className="card">
-          <h3>Next.js Learn &rarr;</h3>
-          <p>Learn about Next.js by following an interactive tutorial!</p>
-        </a>
-        <a
-          href="https://github.com/zeit/next.js/tree/master/examples"
-          className="card"
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Find other example boilerplates on the Next.js GitHub.</p>
-        </a>
+      <div class="buttons">
+        <button class="button is-info is-light">Info</button>
+        <button class="button is-success is-light">Success</button>
+        <button class="button is-warning is-light">Warning</button>
+        <button class="button is-danger is-light">Danger</button>
       </div>
     </div>
 
@@ -83,6 +73,6 @@ const Home = () => (
       }
     `}</style>
   </div>
-)
+);
 
-export default Home
+export default Home;
