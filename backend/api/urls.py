cb95b6d51315db040cbf1ebc2f5acb988d7de7ca
Ken Chen <ken.chen@samsara.com>
2020-01-04 17:30:15 -0800

    backend: Attempt to add basic post journal API, untested

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('journals', views.journal_entries, name='journals'),
     path('reddit', views.reddit_posts, name='reddit')
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -3,13 +3,55 @@ import environ
 import re
 
 from django.http import JsonResponse, HttpResponse
+from django.core import serializers
+
+from .models import JournalEntry
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
+# Journal Entries
+
+
+def journal_entries(request):
+    if request.method == "GET":
+        return get_journal_entries(request)
+    if request.method == "POST":
+        return add_journal_entry(request)
+
+
+def get_journal_entries(request):
+    """
+    List all journal entries.
+
+    Returns:
+      A list of all journal entries
+    """
+    journals = JournalEntry.objects.all()
+    data = serializers.serialize("json", journals)
+    return JsonResponse(data)
+
+
+def add_journal_entry(request):
+    """
+    Add a journal entry.
+
+    Parameters:
+      timestamp (int): When this journal entry should be recorded
+      text (string): The text for this journal entry
+
+    Returns:
+      None
+    """
+    print(request.POST)
+    timestamp = request.POST['timestamp']
+    text = request.POST['text']
+    JournalEntry.objects.create(timestamp=timestamp, text=text)
+
 
 # Reddit
+
 
 def reddit_posts(request):
     """
@@ -17,6 +59,7 @@ def reddit_posts(request):
 
     TODO (jank): Not a model, so no serializer for this.
 
+    Returns:
     ```
     response = {
       "sections": [
@@ -42,7 +85,7 @@ def reddit_posts(request):
     # Most recent sales from frugalmalefashion
     post_regexes = [
         r'\bbanana\srepublic|BR\b',  # Banana Republic
-        r'\bj\s?crew\b',  # JCrew
+        r'\bj[\.\s]?crew\b',  # JCrew
         r'\bbeckett?\ssimonon\b',  # Beckett Simonon
     ]
 
@@ -70,9 +113,9 @@ def reddit_posts(request):
     fmf_section_serialized = serialize_section(
         "Clothing Sales", filtered_posts)
 
-    response = {
+    data = {
         "sections": [
             fmf_section_serialized,
         ]
     }
-    return JsonResponse(response)
+    return JsonResponse(data)
