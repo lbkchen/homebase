61941a51898e5993a68d2c2de471dee7dd8bfca1
Ken Chen <ken.chen@samsara.com>
2020-01-06 22:05:02 -0800

    backend: Allow sending credentials like csrftoken

--- backend/homebase/settings.py
+++ backend/homebase/settings.py
@@ -136,5 +136,9 @@ STATIC_URL = '/static/'
 
 CORS_ORIGIN_WHITELIST = [
     "http://localhost:3000",
-    "http://127.0.0.1:3000"
+    "http://127.0.0.1:3000",
+    "http://localhost:4000",
+    "http://127.0.0.1:4000",
 ]
+
+CORS_ALLOW_CREDENTIALS = True
