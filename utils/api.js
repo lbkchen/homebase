0b8c9df3642f99735ddb23587a784aedbd81f6f0
Ken Chen <ken.chen@samsara.com>
2020-01-20 19:52:26 -0800

    frontend: Try logging cookies for debug

--- utils/api.js
+++ utils/api.js
@@ -15,6 +15,8 @@ class API {
   static postJournalEntry(text) {
     const path = "/api/journals";
     const csrfToken = getCookie("csrftoken");
+    console.log("CSRF", csrfToken);
+    console.log("Cookie", document.cookie);
     return fetch(API.getUrl(path), {
       method: "POST",
       headers: {
