6aa915a3ba8aea04f206a4d659fb42c543eccf93
Ken Chen <ken.chen@samsara.com>
2020-09-07 18:52:11 -0700

    backend: Support Anki stats endpoint

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('journals', views.journal_entries, name='journals'),
-    path('reddit', views.reddit_posts, name='reddit')
+    path('reddit', views.reddit_posts, name='reddit'),
+    path('anki', views.get_anki_stats, name='anki')
 ]
